4a24277906072fd910afde216b37a29c34c0e078
Javier Lopez <javierlopez111@gmail.com>
2024-01-27 19:24:58 -0800

    Adding user serializers and tests views for user module

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,3 +1,7 @@
+"""
+URL mappings for the user API.
+"""
+
 from django.urls import path
 from user import views
 
@@ -6,4 +10,5 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token', views.CreateTokenView.as_view(), name='token'),
 ]
