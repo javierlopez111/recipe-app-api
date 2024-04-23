e4b72e2847b9d1b02249735e2d254f32227415a0
Javier Lopez <javierlopez111@gmail.com>
2024-04-23 00:08:52 -0700

    Updating user files so user is able to update their login

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 """Views for the user API"""
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -17,3 +17,14 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
+
