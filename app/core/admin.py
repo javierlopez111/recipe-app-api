53c6ace77f2743fbea591b03dc47f4ccb2045cc4
Javier Lopez <javierlopez111@gmail.com>
2024-05-02 22:44:34 -0700

    adding config to deploy locally before we go to AWS

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,17 +10,18 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
                 'fields': (
                     'is_active',
                     'is_staff',
-                    'is_superuser'
+                    'is_superuser',
                 )
             }
         ),
-        (_('Important dates'), {'fields': ('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
